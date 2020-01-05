f2b6a5be8873efc1e39bf22ab3039891fb0b965e
Gis70 <ghislainchouet@gmail.com>
2020-01-05 14:24:42 +0100

    Update NextDom_Installer_v1.6.sh

--- NextDom_Installer_v1.6.sh
+++ NextDom_Installer_v1.6.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#2020-01-05 : v1.7 : Correction Typo
 #2019-12-30 : v1.6 : mise en place de fonction
 #2019-11-26 : v1.5 : correction de l'ordre d'installation des dépendances.
 #2019-11-25 : v1.3 : ajout dépôt pour install git + clé
@@ -8,9 +9,9 @@ if [[ $1 == "" ]]; then
     echo
     echo " Usage: "$0" [-git] installation de NextDom via GitClone"
     echo " Usage: "$0" [-apt] installation de NextDom via un paquet Deb"
-    echo " 			[  ] installation depuis le dépôt officel"
-    echo " 			[-d] installation du paquet depuis le dépôt dev"
-    echo " 			[-n] installation du paquet depuis le dépôt nightly"
+    echo " 	       [-apt]	[  ] installation depuis le dépôt officiel"
+    echo " 	       [-apt]	[-d] installation du paquet depuis le dépôt dev"
+    echo " 	       [-apt]	[-n] installation du paquet depuis le dépôt nightly"
     echo
 fi
 
@@ -117,13 +118,8 @@ fi
 
 if [[ $1 == "-?" ]] || [[ $1 == "-help" ]]; then
 		echo ""
-		echo "N A I T v1.6"
+		echo "NextDom Installer tool v1.6"
 		echo ""
-		echo "Nextdom Auto Installer Tool"
-		echo "Developed by the Great Master of Nextdom tEsTs, alias the great @GiDom"
-		echo "with a little help from the appreciative padawan tester, @vinceg77"
-		echo ""
-		echo "Utilisation : nait [OPTION]"
 		echo ""
 		echo "		-apt		: Installation via apt, dépôt officiel"
 		echo "		-apt -n		: version nightly"
